2fe7ca284f62750a9957aab302d4a7719092f4dc
eric0890 <56206244+eric0890@users.noreply.github.com>
2019-10-08 18:52:47 -0700

    Update HW3.py

--- HW3.py
+++ HW3.py
@@ -1,7 +1,7 @@
 #pizzaconverter.py
 #this file converts the price and size of a pizza into cost per square inch
 #note: I tested using 10" diameter and $8 which equals 78.5 square inches divided by 800 cents = 9.81 cents ppsi (rounded)
-import math
+from math import pi
 
 def main():
 
@@ -10,10 +10,9 @@ def main():
     #these formulas are needed to calcuate the final formula(s)
     diameter = int(input("Enter the diameter of the pizza (in inches): "))
     price = int(input("Enter the price of the pizza (in dollars): "))
-    pi = 3.14
     
     #this equation converts the user inputs and combines it with the formula for area
-    areaprice = (math.pi * ((diameter / 2) ** 2) /(price))
+    areaprice = (pi * ((diameter / 2) ** 2) /(price))
     
     print("\nThe cost is", round(areaprice,2), "cents per square inch.")
 
