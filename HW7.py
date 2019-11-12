3e3bb63ea40df32b90b2f3c5c4fbcec778abb067
eric0890 <56206244+eric0890@users.noreply.github.com>
2019-11-11 20:14:15 -0800

    Add files via upload

---
+++ HW7.py
@@ -0,0 +1,29 @@
+#HW7
+#This program produces 3 differently sized circles
+#By Eric Ontiveros
+
+from graphics import *
+
+def main():
+
+    #list x and y coordinates of each circle center
+    centerx = [100,500,800]
+    centery = [100,500,800]
+    radius  = [50, 100, 200]
+
+    for i in range(3):
+        shapes(centerx[i], centery[i], radius[i])
+        print()
+                                   
+def shapes(centerx, centery, radius):
+
+    win =  GraphWin("3 circles", 500, 500)
+    win.setCoords(0,0,1000,1000)
+    center = Point(centerx, centery)
+    circ = Circle(center, radius)
+    circ.setOutline("black")
+    circ.setFill("yellow")
+    circ.draw(win)
+
+
+main()
