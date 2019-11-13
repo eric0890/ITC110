461168d5e1faa277d7a32e1149490ef80d47aa3c
eric0890 <56206244+eric0890@users.noreply.github.com>
2019-11-12 16:32:44 -0800

    Update HW7.py

--- HW7.py
+++ HW7.py
@@ -5,20 +5,20 @@
 from graphics import *
 
 def main():
-
+    
+    win =  GraphWin("3 circles", 500, 500)
+    win.setCoords(0,0,1000,1000)
     #list x and y coordinates of each circle center
     centerx = [100,500,800]
     centery = [100,500,800]
     radius  = [50, 100, 200]
-
+    
     for i in range(3):
-        shapes(centerx[i], centery[i], radius[i])
+        shapes(centerx[i], centery[i], radius[i], win)
         print()
                                    
-def shapes(centerx, centery, radius):
-
-    win =  GraphWin("3 circles", 500, 500)
-    win.setCoords(0,0,1000,1000)
+def shapes(centerx, centery, radius, win):
+   
     center = Point(centerx, centery)
     circ = Circle(center, radius)
     circ.setOutline("black")
